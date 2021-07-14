df9f7f41f15da21b9b8c6d40e6eae4814efdbbf1
Fue Moua <MadisonLifeAgent@gmail.com>
2021-07-14 15:13:14 -0500

    added robots class file with properties.

---
+++ main.py
@@ -0,0 +1,4 @@
+#region - import robot and dinos classes
+
+#endregion
+
---
+++ robots.py
@@ -0,0 +1,12 @@
+#region - initializ robots class and set methods
+
+#robots class
+class robots:
+    #robot properties
+    def __init__(self):
+        self.name = "" # put name in later
+        self.health = 100
+        self.weapon = "" #put weapon name in later
+        self.weapon_attack_power = 10
+
+#endregion
