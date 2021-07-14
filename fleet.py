dc8fa6eb50c28aa9644d1127b8d184b3e09a5061
Fue Moua <MadisonLifeAgent@gmail.com>
2021-07-14 15:29:08 -0500

    added fleet and herd classes

---
+++ fleet.py
@@ -0,0 +1,10 @@
+#region - initializ fleet class and set methods
+
+#fleet class
+class Fleet:
+    #fleet properties
+    def __init__(self):
+        self.names = [] # get from objects
+        self.lives = 3 # one life for each robot in the fleet
+
+#endregion
---
+++ herd.py
@@ -0,0 +1,10 @@
+#region - initializ herd class and set methods
+
+#herd class
+class Herd:
+    #herd properties
+    def __init__(self):
+        self.names = [] # get from objects
+        self.lives = 3 # one life for each dinosaur in the herd
+
+#endregion
