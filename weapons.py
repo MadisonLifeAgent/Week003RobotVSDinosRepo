4f83c270e0a0096de358364008cb4c1156d51ac5
Fue Moua <MadisonLifeAgent@gmail.com>
2021-07-14 15:22:01 -0500

    added weapons class

---
+++ weapons.py
@@ -0,0 +1,10 @@
+#region - initializ weapons class and set methods
+
+#weapons class
+class Weapons:
+    #weapons properties
+    def __init__(self):
+        self.name = "" # put name in later
+        self.attack_power = 10
+
+#endregion
